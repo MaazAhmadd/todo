146bf3ff6c26836ecfae44e18b6b4f35e374fa14
Maaz <maaz.ahmad21399@gmail.com>
2021-07-22 08:08:30 +0500

    sdfs

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ const listSchema = {
 const List = mongoose.model("List", listSchema);
 
 app.get("/download", function (req, res) {
-  res.download("/home/maaz_ahmad213/java-part1/4_comp.zip");
+  res.download("/home/maaz_ahmad213/java-part1/5_comp.zip");
 });
 app.get("/", function (req, res) {
   Item.find({}, function (err, foundItems) {
